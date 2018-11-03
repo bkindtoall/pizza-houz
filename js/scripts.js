3336757ef6cec2539f54a38e3c76819424334881
Edin Filipovic <edin.filipovic89@gmail.com>
2018-11-03 14:31:54 -0700

    added contructors, objects to back office. added radios to ui and now connecting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+//objects being fed through constructor myOrder
 var largeSz = { name: "largeSz", price: 16.99};
 var mediumSz = { name: "mediumSz", price: 13.99};
 var smallSz = { name: "smallSz", price: 10.99};
@@ -16,17 +16,54 @@ var mozzarella = { name: "mozzarella"};
 var feta = { name: "feta"};
 
 
+var myPizza;
+
+function myPizza() {
+	var size
+	var meat
+	var veggie
+	var sauce
+	var cheese
+}
+
+// var pizza;
+
+//pizza constructor
+function Pizza(size, meat, veggie, sauce, cheese) { 
+	this.size = size;
+	this.meat = meat;
+	this.veggie = veggie;
+	this.sauce =sauce;
+	this.cheese = cheese;
+}
+
+// function onlyOne(checkbox) {
+//     var deliveryChk = document.getElementsByName('deliveryOr')
+//     deliveryChk.forEach((item) => {
+//         if (item !== checkbox) item.checked = false
+//     })
+// }
+
+	var deliveryChk = document.getElementsByName('deliveryOr');	
+	var deliveryStatus;
+	
+	for(var i=0; i < deliveryChk.length; i++) {
+        if(deliveryChk[i].checked){
+            deliveryStatus = deliveryChk[i].value;
+        }
+	}        
 
 //User Interface Logic --
 $(document).ready(function() {
-// 	$("#myform input:checkbox").change(function() {
-//     $("#myform input:checkbox").attr("checked", false);
-//     $(this).attr("checked", true);
-// });
-	
+	// $("#customOrder input:checkbox").change(function() {
+	//     $("#customOrder input:checkbox").attr("checked", false);
+	//     $(this).attr("checked", true);
+	// });
+
 	$("form#customOrder").submit(function(event) {
 	event.preventDefault();
 	var firstNm = $("input#first").val();
 	var lastNm = $("input#last").val();
   	});
+  	
 });
