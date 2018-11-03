e9b6adaa1542300b2294a92c98f49493bcdc8acb
Edin Filipovic <edin.filipovic89@gmail.com>
2018-11-02 17:21:44 -0700

    started html with minimal checkboxes and began initial js layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-//behind the scenes
+
+var largeSz = { name: "largeSz", price: 16.99};
+var mediumSz = { name: "mediumSz", price: 13.99};
+var smallSz = { name: "smallSz", price: 10.99};
+
+var bacon = { name: "bacon", price: 1.50};
+var sausage = { name: "sausage", price: 1.50};
+
+var tomatoes = { name: "tomatoes", price: 1.50};
+var mushrooms = { name: "mushrooms", price: 1.50};
+
+var bbq = { name: "bbq"};
+var red = { name: "red"};
+
+var mozzarella = { name: "mozzarella"};
+var feta = { name: "feta"};
 
 
-//user interface
+
+//User Interface Logic --
+$(document).ready(function() {
+
+	$("form#customOrder").submit(function(event) {
+	event.preventDefault();
+	var firstNm = $("input#First").val();
+	var lastNm = $("input#Last").val();
+  	});
+});
